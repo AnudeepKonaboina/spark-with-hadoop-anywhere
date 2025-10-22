c3c024e6ba2e317633130491fc1707a9f48087d9
Anudeep Konaboina <krantianudeep@gamil.com>
2025-10-22 09:04:52 +0530

    Added changes to configs and Dockerfile for hive installation

--- configs/init-hive-db.sh
+++ configs/init-hive-db.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+POSTGRES_PW_FILE="/run/secrets/postgres_password"
+if [ -f "$POSTGRES_PW_FILE" ]; then
+  export PGPASSWORD="$(cat "$POSTGRES_PW_FILE")"
+fi
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE USER hive WITH PASSWORD 'hive';
+  CREATE USER hive WITH PASSWORD '$(cat /run/secrets/postgres_password)';
   ALTER USER hive WITH SUPERUSER;
   CREATE DATABASE metastore;
   GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
@@ -16,7 +21,7 @@ FROM pg_tables
 WHERE tableowner = CURRENT_USER and schemaname = 'public';
   \o
   \i /tmp/grant-privs  
-  CREATE USER hue WITH PASSWORD 'hue';
+  CREATE USER hue WITH PASSWORD '$(cat /run/secrets/postgres_password)';
   ALTER USER hue WITH SUPERUSER;
   CREATE DATABASE hue;
   GRANT ALL PRIVILEGES ON DATABASE hue TO hue;
